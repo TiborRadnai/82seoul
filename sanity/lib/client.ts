d0d63ad23b4be40daf5aacdef12f302d0e94ae61
TiborRadnai <tibor.radnai@icloud.com>
2026-08-13 07:43:00 +0200

    Update client.ts

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from 'next-sanity'
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '8bjbqa5wg',
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId: '8bjbqa5wg', // Hardkódoltuk a project ID-t
+  dataset: 'production',    // Hardkódoltuk a datasetet
   apiVersion: '2024-03-16',
-  useCdn: true,
+  useCdn: false, 
 })
