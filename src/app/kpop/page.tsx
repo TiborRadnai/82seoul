754b256e4e405fb4ee904fccd499a1a0ab5cc9e7
TiborRadnai <tibor.radnai@icloud.com>
2026-08-13 08:45:42 +0200

    Update page.tsx

--- src/app/kpop/page.tsx
+++ src/app/kpop/page.tsx
@@ -6,6 +6,9 @@ import KPopGroupList from '@/components/subpages/kpop/KPopGroupList';
 import KPopGlossaryBubbles from '@/components/subpages/kpop/KPopGlossaryBubbles';
 import GraffitiItem from '@/components/subpages/kpop/GraffitiItem';
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 // Sanity importok (figyelj a visszalépések számára a gyökérben lévő sanity mappáig!)
 import { client } from '../../../sanity/lib/client';
 import { getArtistsQuery } from '../../../sanity/queries';
